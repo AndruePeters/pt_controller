1adc0d3031c8420d4c5469eb3069073a00b5f24b
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2022-05-14 23:48:21 -0400

    Use project gpio c++ library

--- app/main.cpp
+++ app/main.cpp
@@ -6,36 +6,17 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#include <gpio.hpp>
 
-const gpio_num_t BLINK_GPIO = GPIO_NUM_2;
-static uint8_t s_led_state = 0;
-static const char* TAG = "MyModule";
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
+static const char* TAG = "PtController";
 
 extern "C" void app_main(void)
 {
-    /* Configure the peripheral according to the LED type */
-    configure_led();
+    /// Create a GPIO object
+    hal::Gpio led (GPIO_NUM_2, hal::gpio::Mode::output);
 
     while (1) {
-        // ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(5);
+        led.toggle();
+        vTaskDelay(500);
     }
 }
